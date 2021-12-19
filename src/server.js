4c8050d16539f1fb6fdfb29d8439277ce2bc7d72
reid <reidbras@gmail.com>
2021-12-18 21:14:17 -0600

    Adding server code

--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ import http from "http";
 const serverPort = 3000;
 
 export const server = http.createServer((req, res) => {
-
+  res.write("<h1>test</h1");
+  res.end();
 });
 
-server.listen(serverPort, function (err) {
-    console.log(`Error starting server at port ${serverPort}.`);
-    console.log(`Error = ${err}`);
+server.listen(serverPort, function (callback) {
+  console.log(
+    "Server running at url http://" +
+      require("os").hostname() +
+      ":" +
+      serverPort
+  );
 });
 console.log(`Server running on port ${server.address().port}`);
