0c0b95ed1764c5a47c10cdea143535cf95bcd5cb
reid <reidbras@gmail.com>
2021-12-19 11:39:47 -0600

    Adding initial code for readFile

--- src/readFile.js
+++ src/readFile.js
@@ -1,2 +1,23 @@
 //const fs = require("fs");
 import fs from "fs";
+
+export const readFile = function(fileName) {
+    if(fs.existsSync(fileName)) {
+        console.log(`${fileName} exists.`);
+        fs.readFile(fileName, function (err, data) {
+            if(err) {
+                console.log(`An error has occurred reading file ${fileName}.`);
+                console.log(`Error: ${err}`);
+                throw err;
+            }
+            if(data) {
+                console.log(`File ${fileName} successfully read`);
+                console.log(`Data ${data.toString()}`);
+                return data.toString();
+            }
+        });
+    }
+    else {
+        console.log(`${fileName} does not exist.`);
+    }
+}
