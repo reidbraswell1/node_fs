26b61fb9859e53c286263881f733b33544ad409a
reid <reidbras@gmail.com>
2021-12-19 11:41:35 -0600

    Adding readFile import and test code

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
-import readFile from "./readFile.js";
+import { readFile } from "./readFile.js";
 import server from "./server.js";
+const data = readFile('views/index.html');
+console.log(`File contents: ${data}`);
