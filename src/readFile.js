fabc4bd3c5b75d045adf7be95193915cfcc206ab
reid <reidbras@gmail.com>
2021-12-19 20:05:20 -0600

    Adding promise to readFile since fs.readFile is asynchronus

--- src/readFile.js
+++ src/readFile.js
@@ -1,23 +1,28 @@
 //const fs = require("fs");
 import fs from "fs";
 
-export const readFile = function(fileName) {
-    if(fs.existsSync(fileName)) {
-        console.log(`${fileName} exists.`);
-        fs.readFile(fileName, function (err, data) {
-            if(err) {
-                console.log(`An error has occurred reading file ${fileName}.`);
-                console.log(`Error: ${err}`);
-                throw err;
-            }
-            if(data) {
-                console.log(`File ${fileName} successfully read`);
-                console.log(`Data ${data.toString()}`);
-                return data.toString();
-            }
-        });
+// Async function returns a promise
+export const readFile = function (fileName) {
+  // Return a promise that is resolved if when fs.readFile returns data read from 
+  // the file but rejected if the file does not exist or there is a problem 
+  // reading the file.
+  return new Promise(function (resolve, reject) {
+    if (fs.existsSync(fileName)) {
+      console.log(`${fileName} exists. Reading file.`);
+      fs.readFile(fileName, function (err, data) {
+        if (err) {
+          console.log(`An error has occurred reading file ${fileName}.`);
+          console.log(`Error: ${err}`);
+          reject(err);
+        }
+        if (data) {
+          console.log(`File ${fileName} successfully read`);
+          resolve(data.toString());
+        }
+      });
+    } else {
+      console.log(`${fileName} does not exist.`);
+      reject(`${fileName} does not exit.`);
     }
-    else {
-        console.log(`${fileName} does not exist.`);
-    }
-}
+  });
+};
