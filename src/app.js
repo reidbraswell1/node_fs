1c8643fac753cca82001b2354aa6ef76581372c2
reid <reidbras@gmail.com>
2021-12-19 21:31:26 -0600

    Adding code to test file creation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import { readFile } from "./readFile.js";
+import { createFile } from "./create.js";
 import server from "./server.js";
 const data = readFile('views/index.html')
 data.then(function (value)  {
     console.log(`File contents:\n ${value}`);
 }).catch(function (error) {
     console.log(error);
+});
+const create = createFile('views/test.html','<h1>Hello World</h1>');
+create.then(function (message) {
+    console.log(`${message}`)
+}).catch(function (error) {
+    console.log(`${error}`);
 });
