46269cbfe7ee3e9961ebf42472001fa5f0aae29a
reid <reidbras@gmail.com>
2021-12-19 21:30:34 -0600

    Adding code for creation of file

--- src/create.js
+++ src/create.js
@@ -1,2 +1,11 @@
-const fs = require("fs");
+//const fs = require("fs");
 import fs from "fs";
+
+export function createFile(fileName, data) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(fileName, data, 'utf-8', function(error) {
+        reject(error);
+    });
+    resolve(`${fileName} written successfully!`);
+  });
+}
