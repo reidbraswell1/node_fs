fec555f7e7c8e6ff14217806c787c8bd7dd91899
reid <reidbras@gmail.com>
2021-12-20 19:59:35 -0600

    Adding code for appending to file

--- src/updateFile.js
+++ src/updateFile.js
@@ -1,2 +1,21 @@
 //const fs = require("fs");
 import fs from "fs";
+
+export function updateFile(fileName, data) {
+  return new Promise(function (resolve, reject) {
+    if (fs.existsSync(fileName)) {
+      console.log(`${fileName} exists updating`);
+      fs.appendFile(fileName, data, "utf-8", function (err) {
+        if (err) {
+          reject(`Unable to update file ${fileName}`);
+        }
+        else {
+            resolve(`${fileName} was updated`);
+        }
+      });
+    }
+    else {
+        reject(`Unable to update file. ${fileName} does not exist`);
+    }
+  });
+}
