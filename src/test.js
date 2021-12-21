8ef4e19f86c6b1a87d34498515fc88923252a121
reid <reidbras@gmail.com>
2021-12-20 20:01:25 -0600

    Adding code to test update file

--- src/test.js
+++ src/test.js
@@ -2,8 +2,13 @@
 // const createFile = require("./createFile.js");
 import { createFile } from "./createFile.js";
 
+import { updateFile } from "./updateFile.js";
+
 // const readFile = require("./readFile.js");
 import { readFile } from "./readFile.js";
+
+// const deleteFile = require("./deleteFile.js");
+import { deleteFile } from "./deleteFile.js";
 
 export function testCreateFile(testFile, data) {
     createFile(testFile, data)
@@ -14,6 +19,14 @@ export function testCreateFile(testFile, data) {
 
     });
 }
+export function testUpdateFile(testFile, data2) {
+    updateFile(testFile, data2)
+    .then(function (message) {
+        console.log(`${message}`)
+    }).catch(function (error) {
+        console.log(`${error}`);
+    });
+}
 export function testReadFile(testFile) {
     readFile(testFile)
     .then(function (message) {
@@ -22,6 +35,11 @@ export function testReadFile(testFile) {
         console.log(error);
     }); 
 }
-export function testDeleteFile() {
-
+export function testDeleteFile(testFile) {
+    deleteFile(testFile)
+    .then(function (message) {
+        console.log(`${message}`);
+    }).catch(function (error) {
+        console.log(`${error}`);
+    });
 }
