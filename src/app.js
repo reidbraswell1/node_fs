4256cd0224e370e27a4de19d129143efda5e4179
reid <reidbras@gmail.com>
2021-12-22 10:46:31 -0600

    Renaming deconstruct name

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import server from "./server.js";
-import { testCreateFile, testDeleteFile, testReadFile, testUpdateFile } from "./test.js";
+import { testCreateFile, testDeleteFile, testReadFile, testUpdateFile, testReadDirs } from "./test.js";
 
 
 const testFile = "views/test.html";
 const data = "<h1>Test H1</h1><h2>Test H2</h2>";
 const data2 = "<h3>Test H3</h3>";
+const testDirName = "scratchPad";
 
 testCreateFile(testFile, data);
 testUpdateFile(testFile, data2);
 testReadFile(testFile);
 testDeleteFile(testFile);
+testReadDirs(testDirName);
