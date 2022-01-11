55097f796d228d1cd1f785b276077df270ed2d3a
reid <reidbras@gmail.com>
2022-01-11 10:32:23 -0600

    Adding code to populate directory text area

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 //const http = require("http");
 import http from "http";
 import ejs from "ejs";
+import fs from "fs";
+import { readDir } from "./readDirs.js";
 
 const serverPort = 3000;
+let dirPath = "scratchPad";
 
 export const server = http.createServer((req, res) => {
-  let html = ejs.render("<h1><%=heading%></h1>", {heading:"hello"});
+  const template = fs.readFileSync(`./views/index.ejs`, "utf-8");
+  let listing = readDirs(dirPath) + "Test";
+  console.log(`${listing}`);
+  console.log(typeof listing);
+  let html = ejs.render(template, { dirList: `Listing:${listing}Apple` });
   res.write(html);
   res.end();
 });
@@ -19,3 +26,19 @@ server.listen(serverPort, function (callback) {
   );
 });
 console.log(`Server running on port ${server.address().port}`);
+
+function readDirs(path) {
+  console.log(`Path ${path}`);
+  let dirs = "";
+  readDir(path)
+    .then(function (message) {
+      console.log(`${message}`);
+      dirs = message.substring(message.indexOf(":")+2);
+      console.log(dirs);
+      return dirs;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    //return dirs;
+}
