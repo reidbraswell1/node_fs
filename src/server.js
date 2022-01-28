c1d413d13fbf4afef85b022cb27b81e47f633bc0
reid <reidbras@gmail.com>
2022-01-27 20:41:40 -0600

    Adding code to check url and execute code based on url

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ let dirPath = "scratchPad";
 
 export const server = http.createServer((req, res) => {
   console.log("Begin createServer");
-  
+
   let urlToRoute = "";
   let chunks = [];
   // Get the base url without parameters to route the request
@@ -24,6 +24,7 @@ export const server = http.createServer((req, res) => {
 
   let postObject;
   let data = "";
+
   // Handle Request Error's
   req.on("error", (err) => {
     console.log(`Request Error - ${err}`);
@@ -31,38 +32,30 @@ export const server = http.createServer((req, res) => {
     renderErrorPage(req, res, error);
   });
 
-  
-  const template = fs.readFileSync(`./views/index.ejs`, "utf-8");
-  readDir(dirPath)
-    .then(function (message) {
-      // Omit "Read Directories: " text
-      let listing = message.substring(message.indexOf(":") + 2);
-      // Replace all commas with new lines
-      listing = listing.replace(/,/g, "\n");
-      
-      readFile("scratchPad/test-file_1.html")
-        .then(function (message) {
-          let fileContents = message;
-          let html = ejs.render(template, {
-            dirPath: dirPath,
-            dirList: listing,
-            fileContents: message,
-          });
-          console.log("here" + message)
-          res.write(html)
-          res.end()
-        })
-        .catch(function (error) {
-
-        })
-        
-//      res.write(html)
-  //    res.end()
-      })
-    .catch(function (error) {
-
-    });
+  // Handle Response Error's
+  res.on("error", (err) => {
+    console.log(`Response Error - ${err}`);
+    res.writeHead(500, { "Content-Type": "text/html" });
+    renderErrorPage(req, res, err);
   });
+
+  // Assemble data from body of request
+  req.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+
+  req.on("end", () => {
+    console.log(`Chunks`);
+    console.log(chunks.toString());
+    switch (urlToRoute) {
+      case "/":
+        console.log(`--- Begin Case ${urlToRoute} Route ---`);
+        renderHomePage(req, res);
+        console.log(`--- End Case ${urlToRoute} Route ---`);
+        break;
+    }
+  });
+});
 
 server.listen(serverPort, function (callback) {
   console.log(
@@ -73,3 +66,32 @@ server.listen(serverPort, function (callback) {
   );
 });
 console.log(`Server running on port ${server.address().port}`);
+
+function renderHomePage(req, res) {
+  const template = fs.readFileSync(`./views/index.ejs`, "utf-8");
+  readDir(dirPath)
+    .then(function (message) {
+      // Omit "Read Directories: " text
+      let listing = message.substring(message.indexOf(":") + 2);
+      // Replace all commas with new lines
+      listing = listing.replace(/,/g, "\n");
+
+      readFile("scratchPad/test-file_1.html")
+        .then(function (message) {
+          let fileContents = message;
+          let html = ejs.render(template, {
+            dirPath: dirPath,
+            dirList: listing,
+            fileContents: message,
+          });
+          console.log("here" + message);
+          res.write(html);
+          res.end();
+        })
+        .catch(function (error) {});
+
+      //      res.write(html)
+      //    res.end()
+    })
+    .catch(function (error) {});
+}
