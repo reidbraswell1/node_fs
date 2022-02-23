a4a3af5769c4806e3c247b0c2324f3041147ef66
reid <reidbras@gmail.com>
2022-02-22 19:55:06 -0600

    Adding utc and dayjs

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,8 @@
-import { dayjs } from dayjs;
+import dayjs  from 'dayjs';
+import utc  from 'dayjs/plugin/utc';
 
 export function log(method, url, status) {
+    dayjs.extend(utc);
+    let nowUTC = dayjs().utc().format();
+    console.log(`UTC Timestamp = ${nowUTC}`);
 }
