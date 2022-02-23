b3b8b70bdfd24b28217449a7317e24be75e20fc2
reid <reidbras@gmail.com>
2022-02-22 21:31:46 -0600

    Adding changes for logger

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,17 @@
 import dayjs  from 'dayjs';
 import utc  from 'dayjs/plugin/utc';
+import fs from 'fs';
+
+const baseDir = "scratchPad";
+const logName = "log.txt";
 
 export function log(method, url, status) {
     dayjs.extend(utc);
     let nowUTC = dayjs().utc().format();
-    console.log(`UTC Timestamp = ${nowUTC}`);
+    let data = `${method},${url},${status},${nowUTC}\n`;
+    fs.appendFile(`./${baseDir}/${logName}`, data, function(error) {
+        if(error) {
+            console.log(`--- Logger Error Appending To Log File ${error}---`);
+        }
+    });
 }
